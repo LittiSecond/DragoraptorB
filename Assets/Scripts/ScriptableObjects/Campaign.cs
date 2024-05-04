3ae2cfea9e90763c5609b08d75fcf80726b2aa29
LittiSecond <breeze2882@yandex.ru>
2024-05-04 05:41:59 +0300

    begin creating level

---
+++ Assets/Scripts/ScriptableObjects/Campaign.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+namespace Dragoraptor.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "NewCampaign", menuName = "Resources/Campaign")]
+    public sealed class Campaign : ScriptableObject
+    {
+
+        public string[] LevelPaths;
+
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/LevelDescriptor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+namespace Dragoraptor
+{
+    [CreateAssetMenu(fileName = "NewLevel", menuName = "Resources/LevelDescriptor")]
+    public sealed class LevelDescriptor : ScriptableObject
+    {
+        
+        public int LevelId;
+        public GameObject GroundPrefab;
+        public GameObject BackgroundPrefab;
+        //public NpcSpawnRule SpawnRule;
+        [Range(0,1.0f), Tooltip("relative maximum satiety")]
+        public float SatietyToSucces = 0.75f;
+        public float LevelDuration = 180.0f;
+    }
+}
